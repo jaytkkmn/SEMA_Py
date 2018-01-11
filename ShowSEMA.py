88c6d958b036f5865b28b548b07b8116d3ac6cdc
jaytkkmn <jaytkkmn@msn.com>
2018-01-11 17:02:30 +0800

    Initial commit

---
+++ ShowSEMA.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+from subprocess import check_output
+import paho.mqtt.publish as publish
+
+
+# subprocess.run(["./semaeapi_tool", "-a", "SemaEApiBoardGetStringA", "1"]) #doesn't capture output
+
+def MQTT_pub(payload):
+    host = "localhost"
+    topic = "adlink/SEMA"
+    publish.single(topic, payload, qos=1, hostname=host)
+
+
+def getinfo():
+    result = check_output(["./semaeapi_tool", "-a", "SemaEApiBoardGetStringA", "2"]).decode("utf-8")
+
+    return result
+
+
+
+payload = getinfo()
+MQTT_pub(payload)
+
